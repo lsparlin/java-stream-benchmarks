bd36127bac6ec4ea478ec5d1b3a651645eef26e9
Lewis Sparlin <lewis.sparlin@icloud.com>
2017-01-28 12:15:03 -0600

    smaller transformation benchmark

--- src/main/java/com/lewismsparlin/benchmarks/ObjectTransformationBenchmark.java
+++ src/main/java/com/lewismsparlin/benchmarks/ObjectTransformationBenchmark.java
@@ -10,38 +10,38 @@ import com.lewismsparlin.model.MyObj;
 
 public class ObjectTransformationBenchmark implements StreamBenchmark {
 
-	private static final int ITERATIONS = 100000;
+  private static final int ITERATIONS = 10000;
 
-	private static final List<Long> numbers = LongStream.range(0, ITERATIONS).boxed().collect(Collectors.toList());
+  private static final List<Long> numbers =
+      LongStream.range(0, ITERATIONS).boxed().collect(Collectors.toList());
 
-	@Override
-	public void performImperativeBenchmark() {
-		List<MyObj> objects = new ArrayList<>();
+  @Override
+  public void performImperativeBenchmark() {
+    List<MyObj> objects = new ArrayList<>();
 
-		for (Long number : numbers) {
-			MyObj obj = numberToMyObj(number);
+    for (Long number : numbers) {
+      MyObj obj = numberToMyObj(number);
 
-			objects.add(obj);
-		}
+      objects.add(obj);
+    }
 
-		System.out.println("transformed " + ITERATIONS + " numbers into objects: IMPERATIVE");
-	}
+    System.out.println("transformed " + ITERATIONS + " numbers into objects: IMPERATIVE");
+  }
 
-	@Override
-	public void performFunctionalBenchmark() {
+  @Override
+  public void performFunctionalBenchmark() {
 
-		List<MyObj> list = numbers.stream()
-				.map(ObjectTransformationBenchmark::numberToMyObj)
-				.collect(Collectors.toList());
+    List<MyObj> list = numbers.stream().map(ObjectTransformationBenchmark::numberToMyObj)
+        .collect(Collectors.toList());
 
-		System.out.println("transformed " + ITERATIONS + " numbers into objects: FUNCTIONAL");
-	}
+    System.out.println("transformed " + ITERATIONS + " numbers into objects: FUNCTIONAL");
+  }
 
-	private static MyObj numberToMyObj(Long number) {
-		MyObj obj = new MyObj();
-		obj.name = "Object of index " + number;
-		obj.someNumber = number;
-		return obj;
-	}
+  private static MyObj numberToMyObj(Long number) {
+    MyObj obj = new MyObj();
+    obj.name = "Object of index " + number;
+    obj.someNumber = number;
+    return obj;
+  }
 
 }
